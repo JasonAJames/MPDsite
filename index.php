c4ef71e159e7f42088fbcf5b57dc42552ac2661d
Jason James <jasonajames@live.com>
2016-06-15 10:02:11 -0700

    Changed from javascript redirect to PHP header redirect

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php 
-echo '<script type="text/javascript">
-           window.location = "https://myprintingdealswebsite.herokuapp.com/MyPrintingDeals_site/index.php”;
-      </script>';
+<html>
+<?php
+* This will give an error. Note the output
+ * above, which is before the header() call */
+header('Location: https://myprintingdealswebsite.herokuapp.com/MyPrintingDeals_site/index.php');
+exit;
 ?>
