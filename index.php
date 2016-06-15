ed51746331428e27b7a3bc13dc584bbcca2ea16a
Jason James <jasonajames@live.com>
2016-06-15 09:52:04 -0700

    Updated javascript redirect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 echo '<script type="text/javascript">
-           window.location = "https://myprintingdealswebsite.herokuapp.com/MyPrintingDeals_site/index.php”
+           window.location = "https://myprintingdealswebsite.herokuapp.com/MyPrintingDeals_site/index.php”;
       </script>';
 ?>
